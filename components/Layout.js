79caba3db1615c62d0d1de8df6faa27d1ba35aaa
Josh <joshuaprio@fullstack.ph>
2022-11-09 11:33:00 +0800

    Commit testing

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,11 @@
-import Header from "./Header";
+
 import Footer from "./Footer";
 import Topnav from "./Topnav";
 const Layout = ({ children }) => {
     return ( 
         <div className="content">
             <Topnav/>
-            <Header/>
+
             { children }
             <Footer/>
         </div>
