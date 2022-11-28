cfe5438f74d81fc12e36ef9273a8eb79727c316a
adi-fullstack <adi@fullstack.ph>
2022-11-28 17:52:26 +0800

    Product Slider WIP

--- .slicemachine/assets/slices/ProductSlider/index.stories.js
+++ .slicemachine/assets/slices/ProductSlider/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"text_content":[{"type":"paragraph","text":"Aliquip ex exercitation et sint dolore magna nostrud ea officia sunt anim ex officia voluptate commodo. Quis officia est aliquip officia labore deserunt id mollit officia aliqua duis. Est ipsum excepteur sit velit tempor voluptate veniam mollit nulla laborum culpa consectetur.","spans":[]}]}],"primary":{"title":[{"type":"heading1","text":"Captured","spans":[]}],"description":[{"type":"paragraph","text":"Pariatur ipsum reprehenderit exercitation eiusmod ullamco et adipisicing labore adipisicing sint incididunt. Quis ex minim culpa nulla deserunt est labore.","spans":[]}]},"slice_type":"product_slider","id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"product_image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1606248897732-2c5ffe759c04"}}],"primary":{"title":[{"type":"heading1","text":"Wrapped","spans":[]}],"description":[{"type":"paragraph","text":"Magna aute id elit anim veniam culpa magna irure exercitation.","spans":[]}]},"slice_type":"product_slider","id":"_Default"}} />
 _Default.storyName = ''
