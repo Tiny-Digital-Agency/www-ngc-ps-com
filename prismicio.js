0825aa0a2a53e62877e8bde7d384ddf612fff7b0
joshua <joshuaprio@fullstack.ph>
2022-12-21 19:28:34 +0800

    Fetch data valves

--- prismicio.js
+++ prismicio.js
@@ -1,36 +1,26 @@
 import * as prismic from '@prismicio/client'
+import * as prismicH from '@prismicio/helpers'
 import * as prismicNext from '@prismicio/next'
 import sm from './sm.json'
 
-/**
- * The project's Prismic repository name.
- */
 export const repositoryName = prismic.getRepositoryName(sm.apiEndpoint)
 
-// Update the routes array to match your project's route structure
-/** @type {prismic.ClientConfig['routes']} **/
-const routes = [
-  {
-    type: 'homepage',
-    path: '/',
-  },
-  {
-    type: 'page',
-    path: '/:uid',
-  },
-]
+// Update the Link Resolver to match your project's route structure
+export function linkResolver(doc) {
+  switch (doc.type) {
+    case 'homepage':
+      return '/'
+    case 'page':
+      return `/${doc.uid}`
+    case 'valves':
+      return `/products/${doc.uid}`
+    default:
+      return null
+  }
+}
 
-/**
- * Creates a Prismic client for the project's repository. The client is used to
- * query content from the Prismic API.
- *
- * @param config {prismicNext.CreateClientConfig} - Configuration for the Prismic client.
- */
 export const createClient = (config = {}) => {
-  const client = prismic.createClient(sm.apiEndpoint, {
-    routes,
-    ...config,
-  })
+  const client = prismic.createClient(sm.apiEndpoint, config)
 
   prismicNext.enableAutoPreviews({
     client,
